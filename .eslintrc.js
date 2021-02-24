251d8aff867118f480bd29454d52fdc027ef6397
fernando.teixeira <fernandoadt1@gmail.com>
2021-02-24 16:09:40 -0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  globals: {
+    Given: "readonly",
+    When: "readonly",
+    Then: "readonly",
+    expect: "readonly",
+    $$: "readonly",
+    $: "readonly",
+    browser: "readonly",
+    describe: "readonly",
+    it: "readonly",
+    before: "readonly"
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": "warn",
+    "class-methods-use-this": "off",
+  },
+};
--- test/api/specs/scenario3.ts
+++ test/api/specs/scenario3.ts
@@ -1,4 +1,4 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import 'chai/register-should';
 
 require('dotenv').config();
@@ -7,26 +7,26 @@ const request = require('supertest');
 const requestContainer = request(process.env.API_BASEURL);
 const API_PATH = 'cities';
 const testParameters = [
-    {postalCode: 10409, expected: {"Cities":["Berlin"]} },
-    {postalCode: 77716, expected: {"Cities":["Fischerbach", "Haslach", "Hofstetten"]} }
-  ];
+  { postalCode: 10409, expected: { Cities: ['Berlin'] } },
+  { postalCode: 77716, expected: { Cities: ['Fischerbach', 'Haslach', 'Hofstetten'] } },
+];
 
-  describe('Given valid postal codes', () => {
-    testParameters.forEach((parameter) => {
-        it('Should receive 200 status code and corresponding cities names. PostalCode '+parameter.postalCode, (done) => {
-            requestContainer
-            .get(API_PATH+"/"+parameter.postalCode)
-            .set({
-                'Content-Type': 'application/json'
-            })
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
+describe('Given valid postal codes', () => {
+  testParameters.forEach((parameter) => {
+    it(`Should receive 200 status code and corresponding cities names. PostalCode ${parameter.postalCode}`, (done) => {
+      requestContainer
+        .get(`${API_PATH}/${parameter.postalCode}`)
+        .set({
+          'Content-Type': 'application/json',
+        })
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
 
-                expect(res.body).to.be.eql(parameter.expected)
+          expect(res.body).to.be.eql(parameter.expected);
 
-                return done();
-            });
+          return done();
         });
     });
   });
+});
