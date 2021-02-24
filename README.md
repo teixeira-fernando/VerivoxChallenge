1f005eb1a9bf0388eb6783e485af9af1b7d3ac51
fernando.teixeira <fernandoadt1@gmail.com>
2021-02-24 15:23:10 -0300

    Initial setup of API tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VerivoxChallenge
 Project with E2E/UI tests using WebDriverIO and API tests with SuperTest
+
+* API Tests Features:
+    * Parameterized Tests
+    * Environment variables config
+    * HTML Report with Mochawesome
---
+++ test/api/specs/scenario3.ts
@@ -0,0 +1,32 @@
+import { assert, expect } from 'chai';
+import 'chai/register-should';
+
+require('dotenv').config();
+const request = require('supertest');
+
+const requestContainer = request(process.env.API_BASEURL);
+const API_PATH = 'cities';
+const testParameters = [
+    {postalCode: 10409, expected: {"Cities":["Berlin"]} },
+    {postalCode: 77716, expected: {"Cities":["Fischerbach", "Haslach", "Hofstetten"]} }
+  ];
+
+  describe('Given valid postal codes', () => {
+    testParameters.forEach((parameter) => {
+        it('Should receive 200 status code and corresponding cities names. PostalCode '+parameter.postalCode, (done) => {
+            requestContainer
+            .get(API_PATH+"/"+parameter.postalCode)
+            .set({
+                'Content-Type': 'application/json'
+            })
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+
+                expect(res.body).to.be.eql(parameter.expected)
+
+                return done();
+            });
+        });
+    });
+  });
